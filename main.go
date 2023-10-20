28aefdbc0fc65566ac464767d335837ee0be7df5
takato <ltakato@hotmail.com>
2023-10-19 22:31:36 -0300

    variable declarations variations

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var name string = "Takatittos"
+	// var name string = "Takatittos"
+	var name = "Takatittos"    // with type inference
+	short_name := "Takatittos" // short variable declaration operator
 	var age int = 28
 	var version float32 = 1.21
 
 	fmt.Println("Hello, mr.", name, "your age is", age)
 	fmt.Println("This program is in version", version)
+	fmt.Println("The value of short_name is:", short_name)
+
+	// getting infered type
+	fmt.Println("The type of variable age is", reflect.TypeOf(age))
+	fmt.Println("The type of variable version is", reflect.TypeOf(version))
 }
 
 func messageOutput(s string) string {
